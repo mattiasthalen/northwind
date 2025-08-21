2ead236fed316ed9eb9e759766f3b009eb35992c
Mattias Thalén <bitter-polders0x@icloud.com>
2025-08-21 07:27:40 +0000

    Activate all northwind resources

--- dlt/northwind.py
+++ dlt/northwind.py
@@ -29,136 +29,136 @@ def northwind_source() -> t.Any:
                 }
             },
 
-            # {
-            #     "name": "get_northwindapiv_1_categoriesid_details",
-            #     "table_name": "raw__northwind__category_details",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Categories/{id}/Details",
-            #         "params": {
-            #             "id": {
-            #                 "type": "resolve",
-            #                 "resource": "get_northwindapiv_1_categories",
-            #                 "field": "categoryId"
-            #             }
-            #         },
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_categoriesid_details",
+                "table_name": "raw__northwind__category_details",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Categories/{id}/Details",
+                    "params": {
+                        "id": {
+                            "type": "resolve",
+                            "resource": "get_northwindapiv_1_categories",
+                            "field": "categoryId"
+                        }
+                    },
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_customers",
-            #     "table_name": "raw__northwind__customers",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Customers",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_customers",
+                "table_name": "raw__northwind__customers",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Customers",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_employees",
-            #     "table_name": "raw__northwind__employees",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Employees",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_employees",
+                "table_name": "raw__northwind__employees",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Employees",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_employee_territories",
-            #     "table_name": "raw__northwind__employee_territories",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Employees/{id}/Territories",
-            #         "params": {
-            #             "id": {
-            #                 "type": "resolve",
-            #                 "resource": "get_northwindapiv_1_employees",
-            #                 "field": "employeeId"
-            #             }
-            #         },
-            #         "paginator": "single_page"
-            #     },
-            #     "include_from_parent": ["employeeId"]
-            # },
+            {
+                "name": "get_northwindapiv_1_employee_territories",
+                "table_name": "raw__northwind__employee_territories",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Employees/{id}/Territories",
+                    "params": {
+                        "id": {
+                            "type": "resolve",
+                            "resource": "get_northwindapiv_1_employees",
+                            "field": "employeeId"
+                        }
+                    },
+                    "paginator": "single_page"
+                },
+                "include_from_parent": ["employeeId"]
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_ordersid_order_details",
-            #     "table_name": "raw__northwind__order_details",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Orders/{id}/OrderDetails",
-            #         "params": {
-            #             "id": {
-            #                 "type": "resolve",
-            #                 "resource": "get_northwindapiv_1_orders",
-            #                 "field": "orderId"
-            #             }
-            #         },
-            #         "paginator": "single_page"
-            #     }
-            # },
-            # {
-            #     "name": "get_northwindapiv_1_orders",
-            #     "table_name": "raw__northwind__orders",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Orders",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_ordersid_order_details",
+                "table_name": "raw__northwind__order_details",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Orders/{id}/OrderDetails",
+                    "params": {
+                        "id": {
+                            "type": "resolve",
+                            "resource": "get_northwindapiv_1_orders",
+                            "field": "orderId"
+                        }
+                    },
+                    "paginator": "single_page"
+                }
+            },
+            {
+                "name": "get_northwindapiv_1_orders",
+                "table_name": "raw__northwind__orders",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Orders",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_products",
-            #     "table_name": "raw__northwind__products",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Products",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_products",
+                "table_name": "raw__northwind__products",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Products",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_regions",
-            #     "table_name": "raw__northwind__regions",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Regions",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_regions",
+                "table_name": "raw__northwind__regions",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Regions",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_shippers",
-            #     "table_name": "raw__northwind__shippers",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Shippers",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_shippers",
+                "table_name": "raw__northwind__shippers",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Shippers",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_suppliers",
-            #     "table_name": "raw__northwind__suppliers",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Suppliers",
-            #         "paginator": "single_page"
-            #     }
-            # },
+            {
+                "name": "get_northwindapiv_1_suppliers",
+                "table_name": "raw__northwind__suppliers",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Suppliers",
+                    "paginator": "single_page"
+                }
+            },
 
-            # {
-            #     "name": "get_northwindapiv_1_territories",
-            #     "table_name": "raw__northwind__territories",
-            #     "endpoint": {
-            #         "data_selector": "$",
-            #         "path": "Territories",
-            #         "paginator": "single_page"
-            #     }
-            # }
+            {
+                "name": "get_northwindapiv_1_territories",
+                "table_name": "raw__northwind__territories",
+                "endpoint": {
+                    "data_selector": "$",
+                    "path": "Territories",
+                    "paginator": "single_page"
+                }
+            }
         ]
     }
 
